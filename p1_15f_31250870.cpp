878800d80fc70361a5358cd34a7c4479d484d889
Ali <alikal18@gmail.com>
2015-09-30 00:26:24 -0400

    added comments, and made trap state say trap

--- p1_15f_31250870.cpp
+++ p1_15f_31250870.cpp
@@ -13,9 +13,11 @@ string enter;
 string input;
 bool accept = false;
 
-enum State {q0,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,trap};
-State _state;
 
+enum State {q0,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,trap};//state objects
+State _state; //instance
+
+//function headers
 void changeState(State newstate);
 void printState();
 void dump();
@@ -23,20 +25,22 @@ void dump();
 
 int main(){
 
-cout<<"Would you like to enter a string?" <<endl;
+cout<<"Would you like to enter a string?" <<endl; //prompt
 getline(cin, enter);
 while(enter == "yes" || enter == "y")
 {
-cout<<"Okay Whats your string?"<<endl;
-do{getline(cin, input);}while(input.length() < 1);
+cout<<"Okay Whats your string?"<<endl;//prompt
+do{getline(cin, input);}while(input.length() < 1);//accepts string as long as its at least 1 character.
 cout<<"Okay\n"<<"Here is your String: "<<input<<"\n\nAnd Here Is Your Report: "<<endl;
-_state = State::q0;
+changeState(State::q0); //initialize states
 printState();
-for(unsigned int i=0; i < input.length(); i++)
+for(unsigned int i=0; i < input.length(); i++)//loops through each character
 {
-    if(isupper(input[i]) || (!isalpha(input[i])&&input[i] != '.'))
+    if(isupper(input[i]) || (!isalpha(input[i])&&input[i] != '.'))//automatically dumps if char not in L
         changeState(State::trap);
 
+/*Since there is not that many states, i've used a simple switch statement for my dfa.
+this checks each character and the current state and compares them to move along the dfa. */
     cout<<"Character: "<<input[i]<<" ";
     switch(_state)
     {
@@ -53,7 +57,7 @@ for(unsigned int i=0; i < input.length(); i++)
             changeState(State::q2);
         else if(isalpha(input[i]))
             changeState(State::q5);
-        else if(input[i] == '.')
+        else if(input[i] == '.')// for w.com
             changeState(State::q6);
         else dump();
             break;
@@ -63,7 +67,7 @@ for(unsigned int i=0; i < input.length(); i++)
             changeState(State::q3);
         else if(isalpha(input[i]))
             changeState(State::q5);
-        else if(input[i] == '.')
+        else if(input[i] == '.')// for ww.com
             changeState(State::q6);
         else dump();
             break;
@@ -122,7 +126,7 @@ for(unsigned int i=0; i < input.length(); i++)
     printState();
 
 }
-if(_state == State::q9 || _state == State::q12) accept = true;
+if(_state == State::q9 || _state == State::q12) accept = true;//assigns the accepting states
 cout<<"\nString accepted? : "<<std::boolalpha<<accept<<endl;
 
 cout<<"Would you like to enter another string?"<<endl;
@@ -132,6 +136,7 @@ getline(cin, enter);
 
 return 0;
 }
-void changeState(State newstate){_state = newstate;}
-void printState(){cout<<"Current State: q"<< _state <<endl;}
-void dump() {changeState(State::trap); accept = false;}
+
+void changeState(State newstate){_state = newstate;}//changes state
+void printState(){cout<<"Current State: "; _state == State::trap ? cout<<"Trap" : cout<<"q"<<_state;cout<<endl;}//prints state
+void dump() {changeState(State::trap); accept = false;}//Assigns trap state;
